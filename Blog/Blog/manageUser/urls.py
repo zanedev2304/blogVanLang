415e5773750604e6fc24200010331836b70598bb
zanedev2304 <107343249+zanedev2304@users.noreply.github.com>
2023-03-11 21:43:54 +0700

    delete
    Xoá bài project cũ

--- Blog/Blog/manageUser/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import article_list
-
-
-
-urlpatterns = [
-    path('article/',article_list,name='article_list'),
-]
--- Blog/Blog/manageUser/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-
-
-def article_list(request):
-    article = "This is my first article title"
-    return render(request, 'articles.html', {'article':article})
